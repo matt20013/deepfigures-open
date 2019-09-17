73b36f09dd9d7dedd5b8cd9110d775f6b29b81ee
Matthew Hodgskiss <matthew.hodgskiss@md.catapult.org.uk>
2019-09-17 15:43:57 +0100

    Fixes problem with importing imread, imsave from scypy.misc

--- deepfigures/extraction/detection.py
+++ deepfigures/extraction/detection.py
@@ -6,6 +6,8 @@ from typing import List, Tuple, Iterable
 
 import cv2  # Need to import OpenCV before tensorflow to avoid import error
 from scipy.misc import imread, imsave
+from imageio import imread, imsave
+
 import numpy as np
 
 from deepfigures.extraction import (
